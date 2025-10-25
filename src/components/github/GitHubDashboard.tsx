d22161c03f80807da77e51376520206b7f043a7a
Bobby Lim Yun Le <limyunle@gmail.com>
2025-10-25 16:12:02 +0800

    modified dashboards to use aggregate api

--- src/components/github/GitHubDashboard.tsx
+++ src/components/github/GitHubDashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { fetchRepos, Repo } from "../../services/githubService";
+import { fetchRepos, Repo } from "../../services/aggregateService";
 import {
   Box,
   Card,
